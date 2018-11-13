12a52bbc78e7b365ac4f1ae0445fbabedb48d404
ndbinh144 <ndbinh144@gmail.com>
2018-11-13 23:25:53 +0700

    update db and token

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+Vue.use(Vuetify)
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
